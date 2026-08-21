7bb3446ce971d43f0b280adff6b37b4ddd0409f9
João Vitor <joao.vitor.frare@unoesc.edu.br>
2026-08-21 15:35:23 -0300

    Barras de menu finalizado com sucesso

--- script.js
+++ script.js
@@ -3,4 +3,41 @@ const nav = document.querySelector('nav');
 
 botaoMenu.addEventListener('click', function() {
     nav.classList.toggle('menu-aberto');
+    botaoMenu.classList.toggle('menu-aberto');
+    document.body.classList.toggle('menu-aberto');
+});
+
+window.addEventListener('load', function() {
+    document.body.classList.add('pronto');
+});
+
+let timeoutRedimensionar;
+
+window.addEventListener('resize', function() {
+    document.body.classList.add('redimensionando');
+
+    clearTimeout(timeoutRedimensionar);
+    timeoutRedimensionar = setTimeout(function() {
+        document.body.classList.remove('redimensionando');
+    }, 300);
+});
+
+let ultimaPosicao = 0;
+
+window.addEventListener('scroll', function() {
+    const posicaoAtual = window.scrollY;
+
+    if (posicaoAtual > 50) {
+        document.body.classList.add('rolou');
+    } else {
+        document.body.classList.remove('rolou');
+    }
+
+    if (posicaoAtual > ultimaPosicao && posicaoAtual > 100) {
+        document.body.classList.add('escondida');
+    } else {
+        document.body.classList.remove('escondida');
+    }
+
+    ultimaPosicao = posicaoAtual;
 });
